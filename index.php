8e7ef8da0d117fad1db581dd661d5897efd4f438
Samuel Aballa <35260720+sammaserati@users.noreply.github.com>
2022-05-03 22:48:40 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=device-width, intial-scale=1, user-scalable=no">
+	<title>Samuel Aballa</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- favicon -->
+	<link rel="shortcut icon" href="logo.png"/>
+	<script src="https://kit.fontawesome.com/0864d9f9a5.js" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+<body>
+	<!-- main -->
+	<section id="main">
+		<!-- navigation -->
+		<nav>
+			<!-- logo -->
+			<a href="index.php" class="logo"><img src="logo.png"/></a>
+			<!-- menu btn -->
+			<input  class="menu-btn" type="checkbox" id="menu-btn">
+			<label class="menu-icon" for="menu-btn">
+				<span class="nav-icon"></span>
+			</label>
+			<!-- menu -->
+			<ul class="menu">
+				<li><a href="#main">Home</a></li>
+				<li><a href="#skills">Skills</a></li>
+				<li><a href="#recent">Projects</a></li>
+				<li><a href="#service">Services</a></li>
+				<li><a href="#client">Testimonials</a></li>
+				<li><a href="#contact">Contact</a></li>
+			</ul>
+			<a href="mailto: samuelaballa93@gmail.com" class="hey">Say Hi!</a>
+		</nav>
+		<!-- content -->
+		<div class="content">
+			<!-- model -->
+			<div class="model">
+				<img src="profile.png"/>
+			</div>
+			<!-- text -->
+			<div class="main-text">
+				<h1>Hello, I am Samuel</h1>
+				<p>I'm a Full Stack Web developer (both front-end and back-end developer), UX/UI mobile & web designer. I make interactive websites with animations, responsive web & mobile designs with main focus on user experience and solid back-end with secure databases. A responsive design makes your website accessible to all users, regardless of their device. I developed the website you're on in 2 days.<br>I also provide digital marketing services to selective clientell including but not limited to Partner Stack<br>I manage projects of all kinds for organizations, but I mostly enjoy overseeing tech-related projects. If you go through my projects page you'll see I'm best suited to handle your next project due to my team building skills. I know how to gather a cohesive team that'll work as a unit to complete your project in time. The projects listed on this website so far, were exclusively coded (developed) by me.</p>
+				<a href="mailto: samuelaballa93@gmail.com" class="portfolio-btn">Let's Talk About Your Project</a>
+			</div>
+		</div>
+	</section>
+	<!-- skills -->
+	<section id="skills">
+		<!-- text -->
+		<div class="skill-text">
+			<!-- heading -->
+			<div class="skill-heading">
+				<h2>My Expertise</h2>
+			</div>
+			<!-- skill box container -->
+			<div class="s-box-container">
+				<!-- skill box -->
+				<div class="skill-box">
+					<!-- icon -->
+					<div class="s-box-icon">
+						<i class="fab fa-html5"></i>
+					</div>
+					<!-- skill box text -->
+					<div class="s-box-text">
+						<strong>Hyper Text Markup Language (HTML)</strong>
+						<p>I have 3 years experience in HTML especially HTML5 and I have completed a number of HTML5 projects.</p>
+					</div>
+				</div>
+
+				<!-- skill box -->
+				<div class="skill-box">
+					<!-- icon -->
+					<div class="s-box-icon">
+						<i class="fab fa-css3"></i>
+					</div>
+					<!-- skill box text -->
+					<div class="s-box-text">
+						<strong>Cascading Style Sheet (CSS)</strong>
+						<p>I have 3 years experience in CSS3 including CSS frameworks such as Bootstrap.</p>
+					</div>
+				</div>
+
+				<!-- skill box -->
+				<div class="skill-box">
+					<!-- icon -->
+					<div class="s-box-icon">
+						<i class="fab fa-js-square"></i>
+					</div>
+					<!-- skill box text -->
+					<div class="s-box-text">
+						<strong>JavaScript (JS)</strong>
+						<p>I have 2 years experience in JavaScript and I use it to create responsive front-end websites that's focused on the User Experience (UI).</p>
+					</div>
+				</div>
+
+				<!-- skill box -->
+				<div class="skill-box">
+					<!-- icon -->
+					<div class="s-box-icon">
+						<i class="fab fa-php"></i>
+					</div>
+					<!-- skill box text -->
+					<div class="s-box-text">
+						<strong>Hypertext Preprocessor (PHP)</strong>
+						<p>I have 3 years experience in PHP and I have used it in all web development projects I've done thus far especially for back-end development (linking front-end to database).</p>
+					</div>
+				</div>
+
+				<!-- skill box -->
+				<div class="skill-box">
+					<!-- icon -->
+					<div class="s-box-icon">
+						<i class="fas fa-database"></i>
+					</div>
+					<!-- skill box text -->
+					<div class="s-box-text">
+						<strong>Structured Query Language (MySQL)</strong>
+						<p>I have 3 years experience in MySQL and database management systems.</p>
+					</div>
+				</div>
+
+				<!-- skill box -->
+				<div class="skill-box">
+					<!-- icon -->
+					<div class="s-box-icon">
+						<i class="fa-solid fa-layer-group"></i>
+					</div>
+					<!-- skill box text -->
+					<div class="s-box-text">
+						<strong>UI (User Interface) Design</strong>
+						<p>I just gained this skill recently but the designs I've created within that short time are eye-catching. I understand the full UI design process: typography, grids & spacing, forms & UI elements, color, imagery & iconography, design patterns, motion and microinteractions.</p>
+					</div>
+				</div>
+
+				<!-- skill box -->
+				<div class="skill-box">
+					<!-- icon -->
+					<div class="s-box-icon">
+						<i class="fa-brands fa-sketch"></i>
+					</div>
+					<!-- skill box text -->
+					<div class="s-box-text">
+						<strong>UX (User Experience) Design</strong>
+						<p>I have UX design experience on Figma, with full understanding of the UX design process: sketching, userflows, sitemaps, wireframes, prototyping and getting feedback.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- img -->
+		<div class="skill-img">
+			<img src="devv.png"/>
+		</div>
+	</section>
+
+	<!-- projects -->
+	<section class="recent" id="recent">
+		<!-- heading -->
+		<div class="recent-heading">
+			<h3>Most Recent Projects</h3>
+		</div>
+		<!-- img -->
+		<div class="recent-img">
+			<img src="gig2.png"/>
+		</div>
+		<input type="button" class="recent-btn" data-bs-toggle="modal" data-bs-target="#loginModal" value="View Project Designs">
+	</section>
+
+    
+    <!-- Modal -->
+    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+    	<div class="modal-dialog modal-xl">
+    		<div class="modal-content">
+    			<div class="modal-header">
+    				<h5 class="modal-title" id="loginModalLabel" style="color: #36b7b5;">Here are some of the projects I've worked on lately</h5>
+    				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true" aria-label="Close"></button>
+    			</div>
+    			<div class="modal-body">
+    				<div class="container-fluid">
+    					<div class="row">
+    						<div class="slider">
+    							<div class="slides">
+    								<input type="radio" name="radio-btn" id="radio1">
+    								<input type="radio" name="radio-btn" id="radio2">
+    								<input type="radio" name="radio-btn" id="radio3">
+    								<input type="radio" name="radio-btn" id="radio4">
+
+    								<div class="slide first">
+    									<img src="gig1.png" alt="project1"/>
+    								</div>
+    								<div class="slide">
+    									<img src="gig2.png" alt="project2"/>
+    								</div>
+    								<div class="slide">
+    									<img src="gig3.png" alt="project3"/>
+    								</div>
+    								<div class="slide">
+    									<img src="gig4.png" alt="project4"/>
+    								</div>
+    								<!-- automatic navigation -->
+    								<div class="navigation-auto">
+    									<div class="auto-btn1"></div>
+    									<div class="auto-btn2"></div>
+    									<div class="auto-btn3"></div>
+    									<div class="auto-btn4"></div>
+    								</div>
+
+    								<!-- manual navigation -->
+    								<div class="navigation-manual">
+    									<label for="radio1" class="manual-btn"></label>
+    									<label for="radio2" class="manual-btn"></label>
+    									<label for="radio3" class="manual-btn"></label>
+    									<label for="radio4" class="manual-btn"></label>
+    								</div>
+    							</div>
+    							<script type="text/javascript">
+    								var counter = 1;
+    								setInterval(function(){
+    									document.getElementById('radio' + counter).checked = true;
+    									counter++;
+    									if (counter > 4){
+    										counter = 1;
+    									}
+    								}, 5000);
+    							</script>
+    						</div>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="modal-footer">
+    				<button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+
+	<!-- services -->
+	<div class="service" id="service">
+		<div class="title">
+			<h2>My Services</h2>
+		</div>
+		<div class="box">
+			<div class="card">
+				<i class="fa-brands fa-sketch"></i>
+				<h5>UI/UX Mobile & Web Design</h5>
+				<div class="pra">
+					<p>All designs be it mobile or web, should be focused on the visual design just as much as the user experience. Contact me for the breakdown of the entire design process</p>
+					<p style="text-align: center;">
+					</p>
+				</div>
+			</div>
+
+			<div class="card">
+				<i class="fas fa-bars"></i>
+				<h5>Web Development</h5>
+				<div class="pra">
+					<p>Every website should be built to work across all devices, load as fast as possible, have a simple responsive User Experience design and connect to a database without any lag. That is what I focus on</p>
+					<p style="text-align: center;">
+					</p>
+				</div>
+			</div>
+
+			<div class="card">
+				<i class="far fa-user"></i>
+				<h5>Digital Marketing</h5>
+				<div class="pra">
+					<p>Search Engine Optimization (SEO), Keyword research, A/B testing and market research is what I focus on and also researching on the various market trends.</p>
+					<p style="text-align: center;">
+					</p>
+				</div>
+			</div>
+
+			<div class="card">
+				<i class="far fa-bell"></i>
+				<h5>Project Management</h5>
+				<div class="pra">
+					<p>Building a team of people from scratch with skills required to complete a particular project. Co-ordinating the team to work in unison and oversee the progress of the project to completion within the given time frame.</p>
+					<p style="text-align: center;">
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- testimonial -->
+	<section id="client">
+		<!-- heading -->
+		<div class="client-heading">
+			<h3>What Client's Say</h3>
+		</div>
+		<!-- client box container -->
+		<div class="c-box-container">
+			<!-- box 1 -->
+			<div class="client-box">
+				<!-- img -->
+				<img src="client2.jpg"/>
+				<!-- stars/reviews -->
+				<div class="star">
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+				</div>
+				<!--comment -->
+				<strong>Highway Escorts</strong>
+				<p>Timely submission was what we liked the most about his work. Given the timeline we set out, he did a great job within that period. We're happy to be associated with Samuel.</p>
+			</div>
+
+			<!-- box 2 -->
+			<div class="client-box">
+				<!-- img -->
+				<img src="client3.jpg"/>
+				<!-- stars/reviews -->
+				<div class="star">
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+				</div>
+				<!--comment -->
+				<strong>Doublestars Carwash</strong>
+				<p>Mr. Aballa blew our minds with this one, the way he paid attention to all the details and constant communication with our marketing team.</p>
+			</div>
+
+			<!-- box 3 -->
+			<div class="client-box">
+				<!-- img -->
+				<img src="client4.jpg"/>
+				<!-- stars/reviews -->
+				<div class="star">
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="far fa-star"></i>
+				</div>
+				<!--comment -->
+				<strong>Just About Hotels</strong>
+				<p>We needed a simple responsive blog and didn't know where to start, thanks to Samuel our blog is live now and well integrated with Wordpress.</p>
+			</div>
+		</div>
+	</section>
+	<!-- contact -->
+	<section id="contact">
+		<!-- img -->
+		<img src="contact.jpg"/>
+		<!-- heading -->
+		<h3>Get The Best Project Estimation</h3>
+		<!-- input -->
+		<div class="contact-input">
+			<input type="email" placeholder="example@gmail.com"/>
+			<a href="mailto:samuelaballa93@gmail.com">Continue</a>
+		</div>
+	</section>
+	<!-- footer -->
+	<footer>
+		<p>Samuel Aballa</p>
+		<div class="social">
+			<a href="#"><i class="fab fa-facebook-f"></i></a>
+			<a href="https://instagram.com/slimmy_sammy"><i class="fab fa-instagram"></i></a>
+			<a href="https://twitter.com/slimmy_sammy_"><i class="fab fa-twitter"></i></a>
+			<a href="https://www.linkedin.com/in/samuel-aballa-2736b015b"><i class="fab fa-linkedin-in"></i></a>
+		</div>
+		<p class="end">&copy; Copyright 2021 Samuel Aballa</p>
+	</footer>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
